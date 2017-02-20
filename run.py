42e4ba5f6bcebd401eebecf6c2b2c0307aef5761
Nicolas <nicolas@localhost.localdomain>
2017-02-20 10:22:30 -0500

    copy of example 2

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-
+
+import nltk
+from nltk import *
+
+with open ("grammar.cfg", "r") as myfile:
+    grammaireText=myfile.read()
+
+grammar = grammar.FeatureGrammar.fromstring(grammaireText)
+parser = nltk.ChartParser(grammar)
+tokens = "Jean tua Marie avec une corde".split()
+
+
+parser = parse.FeatureEarleyChartParser(grammar)
+trees = parser.parse(tokens)
+for tree in trees:
+    print(tree)
+    nltk.draw.tree.draw_trees(tree)
+    print(tree.label()['SEM'])
