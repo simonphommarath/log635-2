41601de72d2474e9b51ec978849ccea7f4719330
Nicolas <nicolas@localhost.localdomain>
2017-02-27 09:51:00 -0500

    All mighty commit

--- run.py
+++ run.py
@@ -3,17 +3,18 @@
 import nltk
 from nltk import *
 
-with open ("grammar.cfg", "r") as myfile:
+with open ("grammar-final.cfg", "r") as myfile:
     grammaireText=myfile.read()
 
 grammar = grammar.FeatureGrammar.fromstring(grammaireText)
 parser = nltk.ChartParser(grammar)
-tokens = "Jean tua Marie avec une corde".split()
 
 
-parser = parse.FeatureEarleyChartParser(grammar)
-trees = parser.parse(tokens)
-for tree in trees:
-    print(tree)
-    nltk.draw.tree.draw_trees(tree)
-    print(tree.label()['SEM'])
+for phrase in "Le corps est brulé. Han était sur Naboo.".split('.'):
+    tokens = phrase.split()
+    parser = parse.FeatureEarleyChartParser(grammar)
+    trees = parser.parse(tokens)
+    for tree in trees:
+        print(tree)
+        nltk.draw.tree.draw_trees(tree)
+        print(tree.label()['SEM'])
