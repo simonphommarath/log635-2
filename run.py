4ed315405b5949f9299dcc3beddef2092dfe083a
Nicolas <nicolas@localhost.localdomain>
2017-03-04 17:42:02 -0500

    grrrrrreat!

--- run.py
+++ run.py
@@ -3,18 +3,31 @@
 import nltk
 from nltk import *
 
-with open ("grammar-final.cfg", "r") as myfile:
+with open ("grammar.cfg", "r") as myfile:
     grammaireText=myfile.read()
+
+with open('text.txt', 'r') as content_file:
+    content = content_file.read()
 
 grammar = grammar.FeatureGrammar.fromstring(grammaireText)
 parser = nltk.ChartParser(grammar)
 
 
-for phrase in "Le corps est brulé. Han était sur Naboo.".split('.'):
+content = content.replace(', mais', '.')
+#content = content.replace('il', 'Le corps')
+subject = ""
+
+for phrase in content.split('.'):
+    phrase = phrase.replace('il', subject)
+    print(phrase)
     tokens = phrase.split()
     parser = parse.FeatureEarleyChartParser(grammar)
     trees = parser.parse(tokens)
     for tree in trees:
         print(tree)
-        nltk.draw.tree.draw_trees(tree)
-        print(tree.label()['SEM'])
+        #nltk.draw.tree.draw_trees(tree)
+        s = str(tree.label()['SEM'])
+        print(s)
+        subject = s[s.index('subject(') + 8: s.index(')')]
+        print(subject)
+
