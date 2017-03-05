104184b0f90b6810053cc98dfaf61cacb81a1f67
Nicolas <nicolas@localhost.localdomain>
2017-03-04 22:26:00 -0500

    Utilisatio nde pronom personnel

--- run.py
+++ run.py
@@ -12,26 +12,26 @@ with open('text.txt', 'r') as content_file:
 grammar = grammar.FeatureGrammar.fromstring(grammarText)
 parser = nltk.ChartParser(grammar)
 
-print('\n' + story)
+#print('\n' + story)
 
-conjonctions = [', mais',', car',', par contre',', mais']
+conjonctions = [', mais ',', car ',', par contre ',' et ']
 for conjonction in conjonctions:
-    story = story.replace(conjonction, '.')
+    story = story.replace(conjonction, '. ')
 
-print('\n' + story + '\n')
+#print('\n' + story + '\n')
 
 subject = ""
 
 for sentence in story.split('.'):
-    sentence = sentence.replace('il', subject)
+    sentence = sentence.replace(' il ', ' ' + subject + ' ')
     print(sentence)
     tokens = sentence.split()
     parser = parse.FeatureEarleyChartParser(grammar)
     trees = parser.parse(tokens)
     for tree in trees:
         print(tree)
-        #nltk.draw.tree.draw_trees(tree)
+        nltk.draw.tree.draw_trees(tree)
         s = str(tree.label()['SEM'])
         print(s + '\n')
         subject = s[s.index('subject(') + 8: s.index(')')]
-        #print(subject)
+        print(subject + '\n')
