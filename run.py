fe04bdd95c4268538df0dd675dbf6d281647bedb
Simon <simon.phommarath@gmail.com>
2017-03-04 21:25:30 -0500

    Menage de code

--- run.py
+++ run.py
@@ -3,26 +3,35 @@
 import nltk
 from nltk import *
 
+# File opener
 with open ("grammar.cfg", "r") as grammar_file:
     grammarText = grammar_file.read()
 
 with open('text.txt', 'r') as content_file:
     story = content_file.read()
 
+print('\n' + story)
+
+# Text Modifier
+def conjonction_replacer(story):
+    conjonctions = [', mais',', car',', par contre',', mais']
+    for conjonction in conjonctions:
+        story = story.replace(conjonction, '.')
+    return story
+
+def negation_replacer(story):
+    return story
+
+# Story Analyser
 grammar = grammar.FeatureGrammar.fromstring(grammarText)
 parser = nltk.ChartParser(grammar)
 
-print('\n' + story)
+story = conjonction_replacer(story)
 
-conjonctions = [', mais',', car',', par contre',', mais']
-for conjonction in conjonctions:
-    story = story.replace(conjonction, '.')
-
-print('\n' + story + '\n')
-
+sentences = story.split('.')
 subject = ""
 
-for sentence in story.split('.'):
+for sentence in sentences:
     sentence = sentence.replace('il', subject)
     print(sentence)
     tokens = sentence.split()
