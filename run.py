f7c040b211f951b4be4dc5b4df14dbdbaff777dc
YannickMaringo123 <yannickmaringo123@gmail.com>
2017-03-04 21:45:08 -0500

    JessFormattedWorking

--- run.py
+++ run.py
@@ -21,17 +21,31 @@ for conjonction in conjonctions:
 print('\n' + story + '\n')
 
 subject = ""
+theJessTranslatedOutputBodyStart = "(deffact fact \n"
+theJessTranslatedOutputContent = ""
+theJessTranslatedOutputBodyEnd = ")"
 
 for sentence in story.split('.'):
     sentence = sentence.replace('il', subject)
-    print(sentence)
+    #print(sentence)
     tokens = sentence.split()
     parser = parse.FeatureEarleyChartParser(grammar)
     trees = parser.parse(tokens)
+    
     for tree in trees:
-        print(tree)
+        #print(tree)
         #nltk.draw.tree.draw_trees(tree)
         s = str(tree.label()['SEM'])
         print(s + '\n')
+		
+		#JessReformat
+        sStringWithoutClosingBrackets = s[:len(s)-2]        
+        sStringWithoutTheAnd = sStringWithoutClosingBrackets.split('&')
+        NLTKparams = sStringWithoutTheAnd[1].split("(")
+        theJessTranslatedOutputContent = theJessTranslatedOutputContent+"\t("+NLTKparams[0]+" "+NLTKparams[1]+")\n"
+		
         subject = s[s.index('subject(') + 8: s.index(')')]
         #print(subject)
+theJessFormattedOutput = theJessTranslatedOutputBodyStart+theJessTranslatedOutputContent+theJessTranslatedOutputBodyEnd
+with open('jessFormatedOutput.txt','w') as f:
+    f.write(theJessFormattedOutput)
